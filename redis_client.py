018c0908d3bc944639ad12b8ee3c9b0eaa3b5327
ashmit <ashmitbhardwaj05@gmail.com>
2026-02-24 22:51:51 +0530

    fix

--- redis_client.py
+++ redis_client.py
@@ -1,7 +1,7 @@
 import redis
 
 redis_client = redis.Redis(
-    host="clustercfg.test-vector.wjegoz.memorydb.ap-south-1.amazonaws.com:6379",
+    host="clustercfg.test-vector.wjegoz.memorydb.ap-south-1.amazonaws.com",
     port=6379,
     decode_responses=False
 )
